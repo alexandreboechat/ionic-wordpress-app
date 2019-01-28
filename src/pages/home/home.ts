be658e8f6acf1824d261d7c6de2f20ad9a1b7bc0
Alexandre Boechat <ceo@boechat.net>
2019-01-28 21:27:53 -0200

    Load spinner e tratando erro botão mostrar + posts quando número de páginas chegou ao fim.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -10,8 +10,10 @@ import { DetalhePage } from '../detalhe/detalhe';
 export class HomePage {
 
   public itens: any = [];
-  private per_page:number = 8;
-  private page:number = 1;
+  private per_page: number = 8;
+  private page: number = 1;
+  private mostrarCarregarMais = false;
+  private Carregando = false;
 
   constructor(public navCtrl: NavController, public api: ApiProvider, public navParams: NavParams) {
     //log correspondente ao ID de cada categoria
@@ -19,21 +21,37 @@ export class HomePage {
     this.getPosts();
   }
 
-  getPosts(){
-    this.api.get('posts?_embed&per_page='+this.per_page + '&page='+this.page).subscribe((data) => {
-      this.itens = this.itens.concat(data);
-      this.page++;
-    });
+  getPosts() {
+    if (!this.Carregando) {
+      this.Carregando = true;
+      this.api.get('posts?_embed&per_page='+this.per_page +'&page='+this.page)
+        .subscribe((data) => {
+          this.Carregando =  false;
+          this.itens = this.itens.concat(data);
+          if (data.lenght === this.per_page){
+            this.page++;
+            this.mostrarCarregarMais = true;
+          } else {
+            this.mostrarCarregarMais = false;
+          }
+          // Prevenindo erro quando se esgotar o número de páginas para exibição
+        }, (error) => {
+          this.Carregando =  false;
+          if (error.code == "rest_post_invalid_page_number") {
+            this.mostrarCarregarMais = false;
+          }
+        });
+    }
   }
-  
-  abrirDetalhe(item){
-    this.navCtrl.push(DetalhePage, {post:item});
+
+  abrirDetalhe(item) {
+    this.navCtrl.push(DetalhePage, { post: item });
   }
-  
-  pegarNomeCat(cat_id:number){
-    let cat_name:string = '';
+
+  pegarNomeCat(cat_id: number) {
+    let cat_name: string = '';
     this.api.Categorias.forEach(element => {
-      if(element.id==cat_id){
+      if (element.id == cat_id) {
         cat_name = element.name;
       }
     });
